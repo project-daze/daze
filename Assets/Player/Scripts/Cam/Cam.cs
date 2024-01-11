6729b767679e413cd1f23d3b7be7404af9d283e1
Kia King Ishii <kia.king.08@gmail.com>
2024-01-12 02:09:30 +0900

    work work work

--- Assets/Player/Scripts/Cam/Cam.cs
+++ Assets/Player/Scripts/Cam/Cam.cs
@@ -12,17 +12,10 @@ namespace Daze.Player
         public CinemachineFreeLook Look;
         public CinemachineVirtualCamera Free;
 
-        public Transform FollowTarget;
+        public Transform LookFollowTarget;
+        public Transform FreeFollowTarget;
 
-        public float Speed = 200f;
-
-        private enum _camType
-        {
-            Look,
-            Free
-        };
-
-        private _camType _activeCam = _camType.Look;
+        public float FreeCameraRotationSpeed = 200f;
 
         public void OnAwake(Controller controller)
         {
@@ -31,23 +24,56 @@ namespace Daze.Player
 
         public void LateUpdate()
         {
-            FollowTarget.transform.position = Controller.Character.transform.position;
-
-            if (Controller.IsFalling)
+            // If player falling state and active camera do not match, start
+            // transitioning to the correct camera.
+            if (ShouldTransition())
             {
-                Look.gameObject.SetActive(false);
-                _activeCam = _camType.Free;
-            } else
-            {
-                Look.gameObject.SetActive(true);
-                _activeCam = _camType.Look;
+                Look.gameObject.SetActive(!Look.gameObject.activeSelf);
             }
 
+            // Sync follow target position with the player position.
+            LookFollowTarget.transform.position = Controller.Character.transform.position;
+            FreeFollowTarget.transform.position = Controller.Character.transform.position;
+
+            // Update camera rotation.
+            UpdateRotation();
+        }
+
+        private bool IsLookActive()
+        {
+            return Look.gameObject.activeSelf;
+        }
+
+        private bool ShouldTransition()
+        {
+            if (Controller.IsFalling && IsLookActive()) return true;
+            if (!Controller.IsFalling && !IsLookActive()) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Update free camera rotation by rotating follow target. This
+        /// rotation is only required for the free camera.
+        /// </summary>
+        private void UpdateRotation()
+        {
+            // When look camera is active, sync follow target for Free camera
+            // rotation with the main camera (which is Look camera view).
+            // This will make transitioning between cameras look smooth.
+            if (IsLookActive())
+            {
+                FreeFollowTarget.rotation = Main.transform.rotation;
+                return;
+            }
+
+            // Else, it means the Free camera is active. Update its rotation
+            // using the player's input.
             if (Controller.Input.LookComposite == Vector2.zero) return;
 
-            float x = -Controller.Input.LookComposite.y * Speed * Time.deltaTime;
-            float y = Controller.Input.LookComposite.x * Speed * Time.deltaTime;
-            Free.transform.Rotate(x, y, 0f);
+            float x = -Controller.Input.LookComposite.y * FreeCameraRotationSpeed * Time.deltaTime;
+            float y = Controller.Input.LookComposite.x * FreeCameraRotationSpeed * Time.deltaTime;
+
+            FreeFollowTarget.Rotate(x, y, 0f);
         }
     }
 }
